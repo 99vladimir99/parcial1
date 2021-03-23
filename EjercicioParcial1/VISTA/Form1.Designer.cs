9819003bfce81a8f696fe8883051a860553d37fd
DvladimirR <ra0280032020@unab.edu.sv>
2021-03-23 14:56:22 -0600

    .

--- EjercicioParcial1/VISTA/Form1.Designer.cs
+++ EjercicioParcial1/VISTA/Form1.Designer.cs
@@ -56,6 +56,7 @@ namespace EjercicioParcial1
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(775, 240);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // lblEmpleado
             // 
